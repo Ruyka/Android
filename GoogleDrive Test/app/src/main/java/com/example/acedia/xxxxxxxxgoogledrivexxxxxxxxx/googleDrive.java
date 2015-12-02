20546f2574a3206aa8779e3ecf1e87fff2e25c42
Ruyka <knthanh@apcs.com>
2015-12-02 17:22:25 +0700

    upload project

---
+++ GoogleDrive Test/app/src/main/java/com/example/acedia/xxxxxxxxgoogledrivexxxxxxxxx/googleDrive.java
@@ -0,0 +1,11 @@
+package com.example.acedia.xxxxxxxxgoogledrivexxxxxxxxx;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class googleDrive extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
